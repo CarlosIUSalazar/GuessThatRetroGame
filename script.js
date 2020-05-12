804c9d0bc44c88dc429b4a66ce3c8adeeb45e488
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-05-12 23:42:12 +0900

    leaderboards added, styled and connected to main screen

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //Insert .env here
-
+var firebaseConfig = {
+    apiKey: "AIzaSyDIkbRWGDcqaF0UA5KM0P1LFjZsnR5mX_c",
+    authDomain: "guessthatgame-9454c.firebaseapp.com",
+    databaseURL: "https://guessthatgame-9454c.firebaseio.com",
+    projectId: "guessthatgame-9454c",
+    storageBucket: "guessthatgame-9454c.appspot.com",
+    messagingSenderId: "1035180389643",
+    appId: "1:1035180389643:web:21d8c95e71d410cfcc1e0e"
+  };
 
 firebase.initializeApp(firebaseConfig);
 let db = firebase.firestore();
